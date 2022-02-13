6207eb371fa85b2bacfb513d081255404363a9bf
JuliaR16 <86800023+JuliaR16@users.noreply.github.com>
2022-02-13 16:33:09 -0500

    Update home.js

--- home.js
+++ home.js
@@ -52,11 +52,44 @@ var restart =document.getElementById ("restart");
 q1a1.addEventListener("click",correct);
 q1a2.addEventListener("click",incorrect);
 
-q2a1.addEventListener("click",correct);
+q2a1.addEventListener("click",incorrect);
 q2a2.addEventListener("click",incorrect);
+q2a3.addEventListener("click",incorrect);
+q2a4.addEventListener("click",correct);
 
-q3a1.addEventListener("click",correct);
-q3a2.addEventListener("click",incorrect);
+q3a1.addEventListener("click",incorrect);
+q3a2.addEventListener("click",correct);
+q3a3.addEventListener("click",incorrect);
+
+q4a1.addEventListener("click",incorrect);
+q4a2.addEventListener("click",incorrect);
+q4a3.addEventListener("click",correct);
+
+q5a1.addEventListener("click",correct);
+q5a2.addEventListener("click",incorrect);
+
+q6a1.addEventListener("click",incorrect);
+q6a2.addEventListener("click",incorrect);
+q6a3.addEventListener("click",incorrect);
+q6a4.addEventListener("click",correct);
+
+q7a1.addEventListener("click",correct);
+q7a2.addEventListener("click",incorrect);
+
+q8a1.addEventListener("click",correct);
+q8a2.addEventListener("click",incorrect);
+q8a3.addEventListener("click",incorrect);
+q8a4.addEventListener("click",incorrect);
+
+q9a1.addEventListener("click",correct);
+q9a2.addEventListener("click",incorrect);
+q9a3.addEventListener("click",incorrect);
+q9a4.addEventListener("click",incorrect);
+
+q10a1.addEventListener("click",incorrect);
+q10a2.addEventListener("click",correct);
+q10a3.addEventListener("click",incorrect);
+q10a4.addEventListener("click",incorrect);
 
 restart.addEventListener("click",restartQuiz);
 
@@ -65,9 +98,9 @@ function correct () {
   correctAnswer += 1;
   questionCount += 1;
 
-  console.log("questionCount = " + questionCount + " spookyScore = " + spookyScore);
+  console.log("questionCount = " + questionCount + " correctAnswer = " + correctAnswer);
 
-  if (questionCount ==3 ) {
+  if (questionCount ==10 ) {
     console.log ("The quiz is done")
     updateResult ();
   }
@@ -76,18 +109,56 @@ function correct () {
 
 
 function updateResult () {
+  if(correctAnswer >= 0) {
+    result.innerHTML = "You got 0 out of 10 correct";
+    console.log ("You got 0 out of 10 correct");
+    
   if(correctAnswer >= 1) {
-    result.innerHTML = "You got 1 out of 3 correct";
-    console.log ("You got 1 out of 3 correct");
+    result.innerHTML = "You got 1 out of 10 correct";
+    console.log ("You got 1 out of 10 correct");
 
-  } else if (correctAnswer >= 2 ) {
-    result.innerHTML = "You got 2 out of 3 correct";
-    console.log ("You got 2 out of 3 correct");
+  }  if (correctAnswer >= 2 ) {
+    result.innerHTML = "You got 2 out of 10 correct";
+    console.log ("You got 2 out of 10 correct");
   }
+
+  if(correctAnswer >= 3) {
+    result.innerHTML = "You got 3 out of 10 correct";
+    console.log ("You got 3 out of 10 correct");
+
+  } if (correctAnswer >= 4 ) {
+    result.innerHTML = "You got 4 out of 10 correct";
+    console.log ("You got 4 out of 10 correct");
+  }
+
+  if(correctAnswer >= 5) {
+    result.innerHTML = "You got 5 out of 10 correct";
+    console.log ("You got 5 out of 10 correct");
+
+  }  if (correctAnswer >= 6 ) {
+    result.innerHTML = "You got 6 out of 10 correct";
+    console.log ("You got 6 out of 10 correct");
 }
 
+  if(correctAnswer >= 7) {
+    result.innerHTML = "You got 7 out of 10 correct";
+    console.log ("You got 7 out of 10 correct");
+
+  } if (correctAnswer >= 8 ) {
+    result.innerHTML = "You got 8 out of 10 correct";
+    console.log ("You got 8 out of 10 correct");
+  }
+
+  if(correctAnswer >= 9) {
+    result.innerHTML = "You got 9 out of 10 correct";
+    console.log ("You got 9 out of 10 correct");
+
+  }  if (correctAnswer >= 10 ) {
+    result.innerHTML = "You got 10 out of 10 correct. Great Job!";
+    console.log ("You got 10 out of 10 correct");
+  }
+  
 function restartQuiz () {
-  result.innerHTML = "Your result is...";
  correctAnswer = 0;
  questionCount = 0;
 console.log ("questionCount = 0 correctAnswer = 0 ");
@@ -100,10 +171,44 @@ q1a2.addEventListener("click", disableq1);
 
 q2a1.addEventListener("click",disableq2);
 q2a2.addEventListener("click",disableq2);
+q2a3.addEventListener("click",disableq2);
+q2a4.addEventListener("click",disableq2);
 
 q3a1.addEventListener("click",disableq3);
 q3a2.addEventListener("click",disableq3);
+q3a3.addEventListener("click",disableq3);
+    
+q4a1.addEventListener("click",disableq4);
+q4a2.addEventListener("click",disableq4);
+q4a3.addEventListener("click",disableq4);
 
+q5a1.addEventListener("click", disableq5);
+q5a2.addEventListener("click", disableq5);
+    
+q6a1.addEventListener("click",disableq6);
+q6a2.addEventListener("click",disableq6);
+q6a3.addEventListener("click",disableq6);
+q6a4.addEventListener("click",disableq6);
+    
+q7a1.addEventListener("click", disableq7);
+q7a2.addEventListener("click", disableq7);
+    
+q8a1.addEventListener("click",disableq8);
+q8a2.addEventListener("click",disableq8);
+q8a3.addEventListener("click",disableq8);
+q8a4.addEventListener("click",disableq8);
+    
+q9a1.addEventListener("click",disableq9);
+q9a2.addEventListener("click",disableq9);
+q9a3.addEventListener("click",disableq9);
+q9a4.addEventListener("click",disableq9);
+    
+q10a1.addEventListener("click",disableq10);
+q10a2.addEventListener("click",disableq10);
+q10a3.addEventListener("click",disableq10);
+q10a4.addEventListener("click",disableq10);
+
+    
 function disableq1() {
   q1a1.disabled = true; 
   q1a2.disabled = true; 
@@ -112,17 +217,99 @@ function disableq1() {
 function disableq2 () {
 q2a1.disabled = true;
 q2a2.disabled = true;
+q2a3.disabled = true;
+q2a4.disabled = true;
 }
 
 function disableq3 () {
 q3a1.disabled = true;
 q3a2.disabled = true;
+q3a3.disabled = true;
+}
+    
+function disableq4 () {
+q4a1.disabled = true;
+q4a2.disabled = true;
+q4a3.disabled = true;
+}
+    
+function disableq1() {
+q5a1.disabled = true; 
+q5a2.disabled = true; 
+}
+    
+function disableq2 () {
+q6a1.disabled = true;
+q6a2.disabled = true;
+q6a3.disabled = true;
+q6a4.disabled = true;
+}
+    
+function disableq1() {
+q7a1.disabled = true; 
+q7a2.disabled = true; 
+}
+    
+function disableq2 () {
+q8a1.disabled = true;
+q8a2.disabled = true;
+q8a3.disabled = true;
+q8a4.disabled = true;
+}
+    
+function disableq2 () {
+q9a1.disabled = true;
+q9a2.disabled = true;
+q9a3.disabled = true;
+q9a4.disabled = true;
+}
+    
+function disableq2 () {
+q10a1.disabled = true;
+q10a2.disabled = true;
+q10a3.disabled = true;
+q10a4.disabled = true;
 }
 
 function enableQuestions(){
   q1a1.disabled = false; 
   q1a2.disabled = false; 
+  
   q2a1.disabled = false; 
+  q2a3.disabled = false;
+  q2a4.disabled = false; 
   q2a2.disabled = false;
+  
   q3a1.disabled = false; 
   q3a2.disabled = false;
+  q3a3.disabled = false;
+  
+  q4a1.disabled = false; 
+  q4a2.disabled = false;
+  q4a3.disabled = false;
+  
+  q5a1.disabled = false; 
+  q5a2.disabled = false; 
+  
+  q6a1.disabled = false; 
+  q6a3.disabled = false;
+  q6a4.disabled = false; 
+  q6a2.disabled = false;
+  
+  q7a1.disabled = false; 
+  q7a2.disabled = false; 
+  
+  q8a1.disabled = false; 
+  q8a3.disabled = false;
+  q8a4.disabled = false; 
+  q8a2.disabled = false;
+  
+  q9a1.disabled = false; 
+  q9a3.disabled = false;
+  q9a4.disabled = false; 
+  q9a2.disabled = false;
+  
+  q10a1.disabled = false; 
+  q10a3.disabled = false;
+  q10a4.disabled = false; 
+  q10a2.disabled = false;
